f7b35f2624abd217b71baeaf3ea9e5070eb7bbbd
Anezijane <ahnnieanezi@gmail.com>
2024-11-23 06:38:20 +0100

    add hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import Hero from "./Sections/Hero"
 import NavBar from "./Sections/NavBar"
 
 export default function App() {
   return (
     <main className='max-w-7xl mx-auto'>
       <NavBar />
+      <Hero />
     </main>
   )
 }
---
+++ src/Sections/Hero.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export default function Hero() {
+  return (
+    <section className='min-h-screen w-full flex flex-col relative pt-5'>
+        <div className="w-full mx-auto flex flex-col sm:mt-36 mt-20 c-space gap-3">
+            <p className='sm:text-3xl text-2xl font-medium text-white text-center font-generalsans'>Hi, I'm Anezi <span className='waving-hand'>👋</span></p>
+            <p className="hero_tag text-gray_gradient">
+               I Build Products & Brands
+            </p>
+        </div>
+
+        <div className="w-full h-full absolute inset-0">
+
+        </div>
+      
+    </section>
+  )
+}
