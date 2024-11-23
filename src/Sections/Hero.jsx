2a0d05d9b7484d9653dbbbd1f419379278754f83
Anezijane <ahnnieanezi@gmail.com>
2024-11-23 10:41:51 +0100

    add loader

--- src/Sections/Hero.jsx
+++ src/Sections/Hero.jsx
@@ -2,6 +2,7 @@ import React, { Suspense } from 'react'
 import { Canvas } from '@react-three/fiber'
 import { PerspectiveCamera } from '@react-three/drei'
 import HackerRoom from '../Components/HackerRoom'
+import CanvasLoader from '../Components/CanvasLoader'
 
 export default function Hero() {
   return (
@@ -15,12 +16,15 @@ export default function Hero() {
 
         <div className="w-full h-full absolute inset-0">
             <Canvas className='w-full h-full'>
-                <Suspense fallback={null}>
-                    
+                <Suspense fallback={ <CanvasLoader />}>
+                    <PerspectiveCamera makeDefault position={[0,0,30]} />
+                    <HackerRoom scale={0.07} position={[0,0,0]} rotation={[0,280,0]} />
+
+                    <ambientLight  intensity={1}/>
+                    <directionalLight position={[10,10,10]} intensity={0.5} />
                 </Suspense>
 
-                <PerspectiveCamera makeDefault position={[0,0,30]} />
-                <HackerRoom />
+                
             </Canvas>
 
         </div>
