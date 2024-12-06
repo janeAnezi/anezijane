20bca3b059a6fae95b27e116c60d96a525f1a8c6
Anezijane <ahnnieanezi@gmail.com>
2024-12-06 22:42:14 +0100

    add contact section content

--- src/Sections/Contact.jsx
+++ src/Sections/Contact.jsx
@@ -1,19 +1,45 @@
+import { useRef, useState } from "react"
 
 
 export default function Contact() {
+    const formRef = useRef();
+    const [loading, setLoading] = useState(false)
+
+    const [form, setForm] = useState({
+        name: "",
+        email: "",
+        message: ""
+    })
+
+    const handleChange = () => {}
+    const handleSubmit = () => {}
+
   return (
     <section className="c-space my-20" id="contact">
         <div className="relative min-h-screen flex items-center justify-center flex-col">
             <img src="/assets/terminal.png" alt="terminal-bg" className="absolute inset-0 min-h-screen" />
             <div className="contact-container">
                 <h3 className="head-text">Let's talk</h3>
-                <p className="text-lg text-white-600 mt-3">
+                <p className="text-lg text-slate-300 mt-3">
                 Whether you’re looking to build a new website, improve your existing platform, or bring a unique project to
                 life, I’m here to help.
                 </p>
 
-                <form>
-                    
+                <form ref={formRef} onSubmit={handleSubmit}className="mt-12 flex flex-col space-y-7">
+                    <label className="space-y-3">
+                    <span className="field-label">Full Name</span>
+                    <input
+                        type="text"
+                        name="name"
+                        value={form.name}
+                        onChange={handleChange}
+                        required
+                        placeholder="Jane Doe"
+                        className="field-input"
+                    />
+
+                    </label>
+
                 </form>
             </div>
         </div>
