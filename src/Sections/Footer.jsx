20d429be2dd522ef365585bd88ef6c7f205051ca
Anezijane <ahnnieanezi@gmail.com>
2024-12-07 16:55:25 +0100

    add footer socials

--- src/Sections/Footer.jsx
+++ src/Sections/Footer.jsx
@@ -1,21 +1,21 @@
 const Footer = () => {
     return (
       <footer className="c-space pt-7 pb-3 border-t border-black-300 flex justify-between items-center flex-wrap gap-5">
-        <div className="text-white-500 flex gap-2">
+        {/* <div className="text-white-500 flex gap-2">
           <p>Terms & Conditions</p>
           <p>|</p>
           <p>Privacy Policy</p>
-        </div>
+        </div> */}
   
         <div className="flex gap-3">
           <div className="social-icon">
-            <img src="/assets/github.svg" alt="github" className="w-1/2 h-1/2" />
+            <a href="https://github.com/janeAnezi" target="_blank" rel="noopener noreferrer" className="pl-4"><img src="/assets/github.svg" alt="github" className="w-1/2 h-1/2" /></a>
           </div>
           <div className="social-icon">
-            <img src="/assets/twitter.svg" alt="twitter" className="w-1/2 h-1/2" />
+            <a href="http://" target="_blank" rel="noopener noreferrer" className="pl-4"><img src="/assets/twittersvg.jpeg" alt="twitter" className="w-1/2 h-1/2 rounded-lg" /></a>
           </div>
           <div className="social-icon">
-            <img src="/assets/instagram.svg" alt="instagram" className="w-1/2 h-1/2" />
+            <a href="http://" target="_blank" rel="noopener noreferrer" className="pl-4"><img src="/assets/linkdinsvg.png" alt="instagram" className="w-1/2 h-1/2 rounded-lg" /></a>
           </div>
         </div>
   
